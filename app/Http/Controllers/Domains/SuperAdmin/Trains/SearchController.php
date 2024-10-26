f1e5e9c63ca7ee427dbb1aeeeb3b33f3e984a47c
sammykisina <sammykisina@users.noreply.github.com>
2024-10-26 19:19:51 +0000

    Fixes coding style

--- app/Http/Controllers/Domains/SuperAdmin/Trains/SearchController.php
+++ app/Http/Controllers/Domains/SuperAdmin/Trains/SearchController.php
@@ -9,7 +9,6 @@ use Domains\SuperAdmin\Resources\TrainResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use JustSteveKing\StatusCode\Http;
-use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
 
 final class SearchController
@@ -17,7 +16,7 @@ final class SearchController
     public function __invoke(Request $request): Response
     {
         $train  = QueryBuilder::for(subject: Train::class)
-            ->where('service_order',$request->query('service_order'))
+            ->where('service_order', $request->query('service_order'))
             ->allowedIncludes(includes: [
                 'line',
                 'origin',
