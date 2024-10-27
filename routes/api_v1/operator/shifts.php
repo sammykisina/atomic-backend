2bc9b7bd3a4ea4c4c1704d2b9fc004f20d54d015
sammy kisina <sammy.k.mutua@gmail.com>
2024-10-27 13:46:20 +0300

    pint

--- routes/api_v1/operator/shifts.php
+++ routes/api_v1/operator/shifts.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum', 'ability:read-shifts,reject-shifts,accept-shifts,manual-shift-activation'])
     ->prefix('shifts')->as('shifts:')->group(function (): void {
         Route::get('/show', IndexController::class)->name(name: 'shift');
-        Route::get('/panics', ShiftPanicsController::class)->name(name: 'shift-panics'); 
+        Route::get('/panics', ShiftPanicsController::class)->name(name: 'shift-panics');
         Route::patch('/panics/{panic}/acknowledge', AcknowledgePanicController::class)->name(name: 'acknowledge-panic');
 
 
