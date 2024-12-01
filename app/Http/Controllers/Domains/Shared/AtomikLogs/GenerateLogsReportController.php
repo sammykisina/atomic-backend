a053262bd20574e29b31e8a8a9f518312bd89aec
sammykisina <sammykisina@users.noreply.github.com>
2024-12-01 13:50:43 +0000

    Fixes coding style

--- app/Http/Controllers/Domains/Shared/AtomikLogs/GenerateLogsReportController.php
+++ app/Http/Controllers/Domains/Shared/AtomikLogs/GenerateLogsReportController.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Domains\Shared\AtomikLogs;
 
-use Carbon\Carbon;
 use Domains\Shared\Export\AtomikLogsExport;
 use Domains\SuperAdmin\Models\Train;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Excel;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 final class GenerateLogsReportController
 {
@@ -17,9 +15,9 @@ final class GenerateLogsReportController
     {
         return $excel->download(new AtomikLogsExport(
             train_id: $train->id,
-             date: $request->query('date')
+            date: $request->query('date'),
         ), fileName: $train->trainName->name . '-logs.xlsx');
 
-      
+
     }
 }
