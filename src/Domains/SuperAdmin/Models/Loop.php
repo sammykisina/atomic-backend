e46f15d49dd796ae8d87b3daf45bf49fb7774ae4
sammy kisina <sammy.k.mutua@gmail.com>
2025-06-27 10:32:47 +0300

    ft: remove unused fillable properties from Loop model

--- src/Domains/SuperAdmin/Models/Loop.php
+++ src/Domains/SuperAdmin/Models/Loop.php
@@ -13,27 +13,6 @@ final class Loop extends Model
 {
     use HasFactory;
 
-    /** @var array<int, string> */
-    protected $fillable = [
-        'distance',
-        'position',
-        'station_id',
-        'line_id',
-        'status',
-
-        'start_latitude_top',
-        'start_longitude_top',
-
-        'start_latitude_bottom',
-        'start_longitude_bottom',
-
-        'end_latitude_top',
-        'end_longitude_top',
-
-        'end_latitude_bottom',
-        'end_longitude_bottom',
-    ];
-
     /** @return BelongsTo<Station> */
     public function station(): BelongsTo
     {
