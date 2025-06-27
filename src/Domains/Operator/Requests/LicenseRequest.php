bf0d22d5fa880ecd13b70ad0e01b816466ab9d94
sammy kisina <sammy.k.mutua@gmail.com>
2025-06-27 10:32:40 +0300

    ft: add geofence_name validation to origin and destination in LicenseRequest

--- src/Domains/Operator/Requests/LicenseRequest.php
+++ src/Domains/Operator/Requests/LicenseRequest.php
@@ -19,9 +19,11 @@ final class LicenseRequest extends FormRequest
             'origin.origin_id' => ['required', 'integer'],
             'origin.type' => ['required', 'in:STATION,LOOP,SECTION'],
             'origin.coordinates' => ['array','required'],
+            'origin.geofence_name' => ['required'],
 
             'through' => ['array'],
             'through.*.id' => ['integer'],
+            'through.*.geofence_name' => ['nullable'],
             'through.*.type' => ['in:STATION,LOOP,SECTION'],
             'through.*.coordinates' => ['array','required'],
 
@@ -29,24 +31,14 @@ final class LicenseRequest extends FormRequest
             'destination.destination_id' => ['required', 'integer'],
             'destination.type' => ['required', 'in:STATION,LOOP,SECTION'],
             'destination.coordinates' => ['array','required'],
+            'destination.geofence_name' => ['required'],
 
-            'type' => [
-                Rule::enum(type: LicenseTypes::class),
-            ],
+            'type' => [Rule::enum(type: LicenseTypes::class)],
 
-            'line_to_use' => [
-                'required',
-                'in:main_line,trip_line',
-            ],
-            'reason_for_sos_license' => [
-                'required_if:type,SOS',
-            ],
-            'distance_to_stop' => [
-                'required_if:type,SOS',
-            ],
-            'journey_to_be_rescued' => [
-                'required_if:type,SOS',
-            ],
+            'line_to_use' => ['required','in:main_line,trip_line'],
+            'reason_for_sos_license' => ['required_if:type,SOS'],
+            'distance_to_stop' => ['required_if:type,SOS'],
+            'journey_to_be_rescued' => ['required_if:type,SOS'],
         ];
     }
 }
